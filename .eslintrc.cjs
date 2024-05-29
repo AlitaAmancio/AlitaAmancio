4fc55f6e03b634fc6a5edfcee191c5c78b20a69f
AlitaAmancio <amancioalita@gmail.com>
2024-05-29 16:32:20 -0300

    🎉 Begin a project

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+	root: true,
+	'plugins': [
+		'@stylistic/js',
+	],
+	'extends': [
+		'plugin:vue/vue3-essential',
+		'eslint:recommended',
+		'@vue/eslint-config-typescript',
+		'@vue/eslint-config-prettier/skip-formatting',
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+	},
+	rules: {
+		'@stylistic/js/comma-dangle': ['error', 'always-multiline'],
+		'@stylistic/js/eol-last': ['error', 'always'],
+		'@stylistic/js/semi': ['warn', 'never'],
+		'@stylistic/js/indent': ['warn', 'tab'],
+	},
+}
+
+
